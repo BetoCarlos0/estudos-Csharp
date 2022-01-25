a23d2140fb50d58fc1d3a50359e1985c81b19e62
Roberto Carlos <betocarlos00@hotmail.com>
2022-01-25 10:56:21 -0300

    Console C#, arrays

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace estudosCsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] n = new int[5];
+
+            n[0] = 55;
+            n[2] = 30;
+
+            Console.WriteLine(n[0]);
+        }
+    }
+}
+
