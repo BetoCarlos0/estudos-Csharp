982e9a53cc36658db2bc5c7af9f8141d16502d15
Roberto Carlos <betocarlos00@hotmail.com>
2022-02-18 14:31:38 -0300

    arquivos stream

--- arquivos/Helper/FileHelper.cs
+++ arquivos/Helper/FileHelper.cs
@@ -24,5 +24,68 @@ namespace estudos_Csharp.arquivos.Helper
                 WriteLine(item);
             }
         }
+
+        public void CriarDirec(string path)
+        {
+            var direc = Directory.CreateDirectory(path);
+
+            if (direc.Exists)
+            {
+                WriteLine($"Diretório criado: {direc.FullName}");
+                WriteLine($"Nome do diretório: {direc.Name}");
+            }else{
+                WriteLine("Diretório não existe");
+            }
+        }
+
+        public void DeletDirec(string path)
+        {
+            if (Directory.Exists(path))
+            {
+                WriteLine($"Diretório {path} Deletado");
+                Directory.Delete(path);
+                
+            }else{
+                WriteLine("Diretório não existe");
+            }
+        }
+
+        public void CreateFile(string path, string file)
+        {
+            File.WriteAllText(path, file);
+        }
+
+        public void CreateFilestream(string path, List<string> content)
+        {
+            using var stream = File.CreateText(path);
+
+            foreach (var item in content)
+            {
+                stream.WriteLine(item);
+            }
+        }
+
+        public void AddFilestream(string path, List<string> content)
+        {
+            using var stream = File.AppendText(path);
+            
+            foreach (var item in content)
+            {
+                stream.WriteLine(item);
+            }
+        }
+
+        public void ReadFileStream(string path)
+        {
+            using var file = File.OpenText(path);
+
+            var text = string.Empty;
+            
+            while ((text = file.ReadLine()) != null)
+            {
+                WriteLine(text);
+            }
+            //WriteLine(text.ToString());
+        }
     }
 }
