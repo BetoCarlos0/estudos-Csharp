39ecf9bdf0cd60b707636fe8608347483f871dff
Roberto Carlos <betocarlos00@hotmail.com>
2022-09-16 10:41:33 -0300

    modelo sistema celular

---
+++ modelo-sistema/Models/Iphone.cs
@@ -0,0 +1,14 @@
+namespace DesafioPOO.Models
+{
+    public class Iphone : Smartphone
+    {
+        public Iphone(string numero, string modelo, string imei, string memoria) : base(numero, modelo, imei, memoria)
+        {
+        }
+
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine("Aplicativo {0} instaldo no {1}", nomeApp, Modelo);
+        }
+    }
+}
---
+++ modelo-sistema/Program.cs
@@ -0,0 +1,14 @@
+﻿
+using DesafioPOO.Models;
+
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia("123456789", "Nokia 15", "4444444", "124");
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone");
+Smartphone iphone = new Iphone("987654", "Iphone 11", "888888", "212");
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("telegram");
