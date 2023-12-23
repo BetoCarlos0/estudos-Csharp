b57504468f5503c710292d08aca1622a69e5f01f
Roberto Carlos <betocarlos00@hotmail.com>
2023-12-22 23:20:14 -0300

    aprimoramento dos testes com selenium webDriver.

--- Selenium.Tests/Alura.LeilaoOnline.WebApp/wwwroot/js/init.js
+++ Selenium.Tests/Alura.LeilaoOnline.WebApp/wwwroot/js/init.js
@@ -39,69 +39,9 @@ function readPosterURL(input) {
         $('.tooltiped').tooltip();
         $('input[type=text]:not(.browser-default), textarea').characterCounter();
 
-        $('#btnDarLance').on('click', e => {
-            e.preventDefault();
-
-            const lanceOfertado = $('#Valor').val();
-            //enviar requisição com o lance
-            $.post(
-                '/Interessadas/OfertaLance',
-                $('#formDarLance').serialize(),
-                function () {
-                    console.log('lance ofertado!');
-                    M.toast({ html: 'Lance ofertado com sucesso!' });
-
-                    const formatter = new Intl.NumberFormat('pt-BR', {
-                        style: 'currency',
-                        currency: 'BRL',
-                        minimumFractionDigits: 2
-                    });
-
-                    $("#lanceAtual").text(formatter.format(lanceOfertado));
-                }
-            );
-
-        });
-
-        $('.seguir').on('click', e => {
-            e.preventDefault();
-            const link = $(e.target).parent();
-
-            seguirLeilao(
-                $(link).data(),
-                function () {
-                    M.toast({ html: 'Agora você está seguindo o leilão!' });
-                    //mudar o ícone e a classe do link
-                    e.target.textContent = 'star';
-                    $(link).removeClass("seguir");
-                    $(link).addClass("abandonar yellow-text text-darken-4");
-                }
-            );
-        });
-
-        //ação de deixar de seguir um leilão
-        $('.abandonar').on('click', e => {
-            e.preventDefault();
-
-            const link = $(e.target).parent();
-
-            //montar os dados
-            //chamar a função abandonarLeilao com funções de sucesso e erro
-
-            //enviar requisição para deixar de seguir o leilão
-            abandonarLeilao(
-                $(link).data(),
-                function () {
-                    console.log('leilão foi abandonado!');
-                    M.toast({ html: 'Você deixou de seguir o leilão!' });
-                    //mudar o ícone e a classe do link
-                    e.target.textContent = 'star_border';
-                    $(link).removeClass("abandonar yellow-text text-darken-4");
-                    $(link).addClass("seguir white-text");
-                }
-            );
-
-        });
+        $('#btnDarLance').on('click', darLance);
+        $('.seguir').one('click', clickSeguir);
+        $('.abandonar').one('click', clickAbandonar);
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
